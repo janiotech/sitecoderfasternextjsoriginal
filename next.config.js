79840ec0bb666cc9263d0bad2aa8c1abe650264b
DarkS <sk8.merp@gmail.com>
2022-06-09 14:33:34 -0300

    first

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    loader: 'inginx',
+    loader: 'imgix',
     path: 'https://www.coderfaster.com.br/',
   },
   reactStrictMode: true,
